c187c06d890027a31d1aa5d55842fd77f69ba288
vicsenedesse <engsoftvicsenedesse@gmail.com>
2021-11-29 16:39:13 -0400

    refatorando o teste

--- src/test/java/com/jenkins/springboot/controller/ProdutoControllerTeste.java
+++ src/test/java/com/jenkins/springboot/controller/ProdutoControllerTeste.java
@@ -2,30 +2,49 @@ package com.jenkins.springboot.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import io.restassured.http.ContentType;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.*;
+import com.jenkins.springboot.repository.ProdutoRepository;
+import com.jenkins.springboot.model.Produto;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+ 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class ProdutoControllerTeste {
+
+  @Autowired
+  private MockMvc mockMvc;
     
-    @Autowired
-    private ProdutoController produtoController;
+  @InjectMocks
+  private ProdutoController produtoController;
 
-    @BeforeEach
-    public void setup(){
-        standaloneSetup(this.produtoController);
+  @BeforeEach
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(produtoController).build();
     }
 
-    @Test
-    public void deveRetornarSucesso_QuandoBuscarProduto(){
-      .given()
-        .accept(ContentType.JSON)
-      .when()
-        .get("/api/produtos/{codigo}", 1L)
-      .then()
-        .statusCode(HttpStatus.OK.value());
-    }
+  @Test
+  public void testGetAll() throws Exception{
+    mockMvc.perform(get("/api/produto/")
+      .accept(MediaType.APPLICATION_JSON))
+      .andExpect(status().isOk())
+      .andExpect(content().json("[]"));
+  }
 }
